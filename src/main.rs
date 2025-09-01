6d88fef9d9647e1a0ad68dc937acc392a1072e2f
俺不叫化咕龙 <1801943622@qq.com>
2025-09-01 20:27:10 +0800

    Refactor process handling in main function and enhance Process struct to retrieve process names

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
-use std::ptr::NonNull;
+use std::{mem::MaybeUninit, ptr::NonNull};
 
-fn main() {
-    dbg!(enum_proc().unwrap().len());
+fn main() -> anyhow::Result<()> {
+    enum_proc()?.into_iter().for_each(|pid| {
+        let process = match Process::open(pid) {
+            Ok(process) => process,
+            Err(e) => {
+                println!("{e}");
+                return;
+            }
+        };
+        let name = match process.name() {
+            Ok(name) => name,
+            Err(e) => {
+                println!("Failed to get process name: {e}");
+                return;
+            }
+        };
+        println!("{}: {}", pid, name);
+    });
+    Ok(())
 }
 
 pub fn enum_proc() -> anyhow::Result<Vec<u32>> {
@@ -31,7 +48,7 @@ impl Process {
     pub fn open(pid: u32) -> anyhow::Result<Self> {
         let handle = unsafe {
             winapi::um::processthreadsapi::OpenProcess(
-                winapi::um::winnt::PROCESS_QUERY_INFORMATION,
+                winapi::um::winnt::PROCESS_QUERY_INFORMATION | winapi::um::winnt::PROCESS_VM_READ,
                 winapi::shared::minwindef::FALSE,
                 pid,
             )
@@ -39,6 +56,37 @@ impl Process {
         let handle = NonNull::new(handle)
             .ok_or_else(|| anyhow::anyhow!("Failed to open process: {}", pid))?;
         Ok(Process { pid, handle })
+    }
+
+    pub fn name(&self) -> anyhow::Result<String> {
+        let mut module = MaybeUninit::uninit();
+        let mut size = 0;
+        if unsafe {
+            winapi::um::psapi::EnumProcessModules(
+                self.handle.as_ptr(),
+                module.as_mut_ptr(),
+                size_of::<winapi::shared::minwindef::HMODULE>() as u32,
+                &raw mut size,
+            ) == winapi::shared::minwindef::FALSE
+        } {
+            return Err(std::io::Error::last_os_error().into());
+        }
+
+        let module = unsafe { module.assume_init() };
+        let mut buffer = MaybeUninit::<[u16; 64]>::uninit();
+        let length = unsafe {
+            winapi::um::psapi::GetModuleBaseNameW(
+                self.handle.as_ptr(),
+                module,
+                buffer.as_mut_ptr().cast(),
+                64,
+            )
+        };
+        if length == 0 {
+            return Err(std::io::Error::last_os_error().into());
+        }
+        let buffer = unsafe { buffer.assume_init() };
+        Ok(String::from_utf16_lossy(&buffer[..length as usize]))
     }
 }
 
