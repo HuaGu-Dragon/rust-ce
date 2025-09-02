33f65603efccbea35611934b55755cb6ba7300da
俺不叫化咕龙 <1801943622@qq.com>
2025-09-02 13:27:39 +0800

    Enhance Process struct with memory management methods and update cSpell settings

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,8 @@ fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
+//TODO: change the buffer to user given, like the Read trait
+//TODO: change vector to a more abstract type, like a slice or other type like HashMap
 pub fn enum_proc() -> anyhow::Result<Vec<u32>> {
     let mut pid = Vec::with_capacity(1024);
     let mut size = 0;
@@ -87,6 +89,44 @@ impl Process {
         }
         let buffer = unsafe { buffer.assume_init() };
         Ok(String::from_utf16_lossy(&buffer[..length as usize]))
+    }
+
+    pub fn memory_region(&self) -> anyhow::Result<winapi::um::winnt::MEMORY_BASIC_INFORMATION> {
+        let mut info = MaybeUninit::uninit();
+
+        let written = unsafe {
+            winapi::um::memoryapi::VirtualQueryEx(
+                self.handle.as_ptr(),
+                std::ptr::null(),
+                info.as_mut_ptr(),
+                size_of::<winapi::um::winnt::MEMORY_BASIC_INFORMATION>(),
+            )
+        };
+        if written == 0 {
+            Err(std::io::Error::last_os_error().into())
+        } else {
+            Ok(unsafe { info.assume_init() })
+        }
+    }
+
+    pub fn read_memory(&self, addr: usize, n: usize) -> anyhow::Result<Vec<u8>> {
+        let mut buffer: Vec<u8> = Vec::with_capacity(n);
+        let mut read = 0;
+        if unsafe {
+            winapi::um::memoryapi::ReadProcessMemory(
+                self.handle.as_ptr(),
+                addr as _,
+                buffer.as_mut_ptr().cast(),
+                buffer.capacity(),
+                &mut read,
+            ) == winapi::shared::minwindef::FALSE
+        } {
+            return Err(std::io::Error::last_os_error().into());
+        }
+        unsafe {
+            buffer.set_len(read);
+        }
+        Ok(buffer)
     }
 }
 
