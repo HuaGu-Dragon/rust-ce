08e36b68522ee89780e4aba11197245cb8c55ead
俺不叫化咕龙 <1801943622@qq.com>
2025-09-05 11:45:00 +0800

    Add ProcessThread struct with methods for opening, suspending, and resuming threads

--- src/debug.rs
+++ src/debug.rs
@@ -1,4 +1,4 @@
-use std::mem::MaybeUninit;
+use std::{mem::MaybeUninit, ptr::NonNull};
 
 pub struct Debugger {
     handle: winapi::um::winnt::HANDLE,
@@ -16,6 +16,7 @@ pub fn enum_threads(pid: u32) -> anyhow::Result<Vec<u32>> {
     const ENTRY_SIZE: u32 = std::mem::size_of::<winapi::um::tlhelp32::THREADENTRY32>() as u32;
     const NEEDED_ENTRY_SIZE: u32 =
         4 * std::mem::size_of::<winapi::shared::minwindef::DWORD>() as u32;
+
     let handle = unsafe {
         winapi::um::tlhelp32::CreateToolhelp32Snapshot(winapi::um::tlhelp32::TH32CS_SNAPTHREAD, 0)
     };
@@ -50,3 +51,51 @@ pub fn enum_threads(pid: u32) -> anyhow::Result<Vec<u32>> {
 
     Ok(threads)
 }
+
+pub struct ProcessThread {
+    tid: u32,
+    handle: NonNull<winapi::ctypes::c_void>,
+}
+
+impl ProcessThread {
+    pub fn open(tid: u32) -> anyhow::Result<Self> {
+        match NonNull::new(unsafe {
+            winapi::um::processthreadsapi::OpenProcess(
+                winapi::um::winnt::THREAD_SUSPEND_RESUME,
+                winapi::shared::minwindef::FALSE,
+                tid,
+            )
+        }) {
+            Some(handle) => Ok(ProcessThread { tid, handle }),
+            None => Err(std::io::Error::last_os_error().into()),
+        }
+    }
+
+    pub fn suspend(&mut self) -> anyhow::Result<usize> {
+        let ret = unsafe { winapi::um::processthreadsapi::SuspendThread(self.handle.as_ptr()) };
+        if ret == -1i32 as u32 {
+            Err(std::io::Error::last_os_error().into())
+        } else {
+            Ok(ret as usize)
+        }
+    }
+
+    pub fn resume(&mut self) -> anyhow::Result<usize> {
+        let ret = unsafe { winapi::um::processthreadsapi::ResumeThread(self.handle.as_ptr()) };
+        if ret == -1i32 as u32 {
+            Err(std::io::Error::last_os_error().into())
+        } else {
+            Ok(ret as usize)
+        }
+    }
+}
+
+impl Drop for ProcessThread {
+    fn drop(&mut self) {
+        unsafe {
+            winapi::um::handleapi::CloseHandle(self.handle.as_mut());
+        }
+    }
+}
+
+impl ProcessThread {}
